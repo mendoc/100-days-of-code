58d1760f52a12029185a31d5157054d21d13f176
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2021-03-14 22:43:24 +0000

    Jour 11

---
+++ timeline/jour-11/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import 'semantic-ui-css/semantic.min.css'
+import './App.css'
+import { Container, Divider, Header } from 'semantic-ui-react'
+
+class App extends Component {
+    render() {
+        return (
+            <Container>
+                <Header as='h2'>Jour 11</Header>
+                <p>Description du jour</p>
+                <Divider />
+            </Container>
+        )
+    }
+}
+
+export default App;
