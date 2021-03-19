974d5401a1a8995705a9dc30a6018053dae861f6
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2021-03-19 15:59:42 +0000

    Jour 16 : fix: liens des posts cliquables

--- timeline/jour-15/src/components/Post.jsx
+++ timeline/jour-15/src/components/Post.jsx
@@ -1,10 +1,11 @@
 import Moment from 'react-moment';
+import Linkify from 'react-linkify';
 
 export default function Post({ data }) {
     return (
         <div className="card">
             <div className="card-body">
-                <p className="card-text">{data.content}</p>
+                <p className="card-text"><Linkify>{data.content}</Linkify></p>
                 <blockquote className="blockquote mb-0">
                     <footer className="blockquote-footer">
                         <small className="text-muted">
